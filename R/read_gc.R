cc12f91babac6c5d884271903110ea7b20e8692e
debotaniker <patrickaurich1996@googlemail.com>
2023-07-18 09:45:03 +0200

    finished the read_gc function. Works now for first example files

--- R/read_gc.R
+++ R/read_gc.R
@@ -1,29 +1,94 @@
-#' Brief description of the function
+#' Function to read data from the GC2 template.
 #'
-#' More detailed description of the function.
-#' This can span multiple lines if needed.
+#' This function can be used to read data from the GC2 template file. It takes sample information from the "results" sheet
+#' and measurement data from "concentration" sheet. Parameter for the function is only the files name.
 #'
-#' @param arg1 Description of the first argument
-#' @param arg2 Description of the second argument
-#' @return Description of the return value
-#' @keywords keyword1 keyword2
+#' @param filename Filename of the datafile.
+#' @param egm_model Character, which contains the model number of the EGM CO2 analyzer.
+#' @return Clean datafile of the measurements.
+#' @keywords gc co2 ch4 n2o o2 headspace
 #' @export
 #' @examples
 #' # Example usage of the function
-#' my_function(arg1 = "value1", arg2 = 10)
+#' read_gc(filename = "path\\to\\file.txt")
 #'
-#' @importFrom package_name function1 function2
-#' @import package_name
-#' @importFrom package_name2 function3
-#' @import package_name2
-#' @importFrom package_name3 function4
-#' @import package_name3
+#' @importFrom readxl read_excel
+#' @import readxl
 
-read_gc <- function(arg1, arg2) {
-  # Function implementation code goes here
-  # ...
-  # ...
+library(readxl)
 
-  # Return the result
+read_gc <- function(filename) {
+  dat <-
+    read_excel(filename,
+               sheet = "concentration",
+               skip = 7,
+               col_names = FALSE)
+
+  header <- c(
+    "Position",
+    "Sample_id",
+    "Temperature_equilibration",
+    "Datetime",
+    "CH4_µmol_l",
+    "N2O_µmol_l",
+    "CO2_corrected_µmol_l",
+    "O2_µmol_l",
+    "CO2_simple_µmol_l",
+    "CO1_simple_r_µmol_l",
+    "empty",
+    "Bunsen_CH4",
+    "Bunsen_CO2",
+    "Bunsen_N2O",
+    "Bunsen_O2",
+    "Air_CH4_ppm",
+    "Air_N2O_ppm",
+    "Air_CO2_ppm",
+    "Air_O2_percent"
+  )
+
+  colnames(dat) <- header
+
+  dat <- dat[dat$Sample_id != 0, ]
+
+  dt <- dat$Datetime
+
+  results <-
+    read_excel(filename,
+               sheet = "results",
+               skip = 7,
+               col_names = FALSE)[, c(1:13)]
+
+  header_results <- c(
+    "Position",
+    "Label",
+    "Sample_id",
+    "Temperature_equilibration",
+    "Air_pressure_mbar",
+    "Temperature_insitu",
+    "pH",
+    "Datetime",
+    "GC_file_name",
+    "Headspace_CH4_ppm",
+    "Headspace_CO2_ppm",
+    "Headspace_N2O_ppm",
+    "Headspace_O2_percent"
+  )
+
+  colnames(results) <- header_results
+
+  results <- results[!is.na(results$Sample_id), ]
+
+  result <- data.frame(
+    Datetime = dt,
+    Method = "Headspace_equilibration",
+    results[, !names(results) %in% c("Datetime")],
+    dat[, !names(dat) %in% c("Position",
+                             "Sample_id",
+                             "Temperature_equilibration",
+                             "Datetime",
+                             "empty")],
+    row.names = NULL
+  )
+
   return(result)
 }
